ec7118cd68d505b5da19a7155fb4fc5ea7048db0
Wahab-a-web <abdul.iburahim@student.ateneo.edu>
2025-03-01 20:26:15 +0800

    Added data models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+class RecipeIngredient(models.Model):
+  
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='ingredient',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipe',
+    )
+    def get_absolute_url(self):
+        return reverse('task_detail', args=[str(self.name)])
