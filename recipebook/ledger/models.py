6db1014be81b2ba348dcc3b3753ff85d5bdfa641
Wahab-a-web <abdul.iburahim@student.ateneo.edu>
2025-03-10 21:06:02 +0800

    Password reset feature added

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.shortcuts import render
 
 # Create your models here.
 class Ingredient(models.Model):
