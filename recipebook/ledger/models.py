2d75f89d66425dfa53e779c1ffc8c365c247db70
Wahab-a-web <abdul.iburahim@student.ateneo.edu>
2025-03-25 11:14:21 +0800

    cleaned some blank space

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
-from django.shortcuts import render
 
 
 class Ingredient(models.Model):
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -4,7 +4,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Recipe, RecipeImage
 from .forms import RecipeForm
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 
