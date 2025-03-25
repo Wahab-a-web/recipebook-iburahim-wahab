5f9983b2cca8efa09924c05dfd7bf00ff24caf28
Wahab-a-web <abdul.iburahim@student.ateneo.edu>
2025-03-25 08:03:32 +0800

    Create New Recipe feature added

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index, RecipeDetailView, RecipesListView
+from .views import index, RecipeDetailView, RecipesListView, RecipeCreateView, RecipeUpdateView
 
 urlpatterns = [
     path('', index, name='index'),
     path('recipes/list', RecipesListView.as_view(), name='recipes/list'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail'),
+    path('recipe/<int:pk>', RecipeUpdateView.as_view(), name='recipe-detail'),
+    path('recipe/add', RecipeCreateView.as_view(), name='recipe-add'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 app_name = 'ledger'
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Recipe
+from .forms import RecipeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -14,8 +16,34 @@ class RecipesListView(ListView):
     model = Recipe
     template_name = 'recipe_book.html'
 
+    def post(self, request, *args, **kwargs):
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return self.get(request, *args, **kwargs)
+        else:
+            self.object_list = self.get_queryset(**kwargs)
+            context = self.get_context_data(**kwargs)
+            context['form'] = form
+            return self.render_to_response(context)
+
+
+
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
+    template_name = 'recipe.html'
+    redirect_field_name = 'recipes/list'
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    fields = '__all__'
+    template_name = 'add_recipe.html'
+
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    fields = '__all__'
     template_name = 'recipe.html'
     redirect_field_name = 'recipes/list'
